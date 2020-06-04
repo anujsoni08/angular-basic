ad251e18bf53168c2a1ad8025daf2d64f34ebd64
Anuj Soni <anujsoni08@gmail.com>
2020-06-04 11:35:21 +0530

    basic angular hostlistener added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angular-basic';
+  title = "angular-basic";
+  innerHeight = window.innerHeight;
+
+  @HostListener("window:resize", ["$event"])
+  onResize(event) {
+    console.log(event.target.innerWidth);
+    this.innerHeight = event.target.innerWidth;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YellowBackgroundDirective } from './directives/yellowBackgroundDirective/yellow-background.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YellowBackgroundDirective
   ],
   imports: [
     BrowserModule,
